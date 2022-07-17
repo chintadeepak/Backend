b4d40d5ab3a3d62fa6e5d4445012a9ee58e283df
unaveengoud <unaveengoud.006@gmail.com>
2022-07-17 08:40:51 +0530

    pravahlika

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -32,7 +32,7 @@ public class PackersMoversController {
         @GetMapping("/ping")
         public String ping() {
 
-        	return "Hi this Vishnu";
+        	return "Hi this Pravahlika";
         }
 
 
